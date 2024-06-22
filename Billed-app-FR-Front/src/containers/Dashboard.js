1d8e212afe0576ef5da94fb5fe985a2c7979621a
Nick Pseudo <tomdop.dakar@gmail.com>
2024-06-22 10:22:29 +0200

    better fix for admin dashboard

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -86,15 +86,25 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    console.log(this);
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
+    if (this.counter % 2 === 0) {
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
+      this.counter ++
+    } else {
+      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+
+      $('.dashboard-right-container div').html(`
+        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+      `)
+      $('.vertical-navbar').css({ height: '120vh' })
+      this.counter ++
+    }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -121,29 +131,6 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.index === undefined || this.index !== index) this.index = index
-    $(`#arrow-icon${this.index}`).toggleClass("displayed")
-    if ($(`#arrow-icon${this.index}`).hasClass("displayed")) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-    }
-
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
-    return bills
-
-  }
-
-
-  /*
-  handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
@@ -159,13 +146,21 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      if (bill) {
+      const billDOM = document.querySelector(`#open-bill${bill.id}`)
+      if (billDOM) {
+        const container = billDOM.parentNode;
+      if (container.getAttribute('id') == (`status-bills-container${index}`)) {
+        $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+        }
+      }
+    }
     })
 
     return bills
 
   }
-*/
+
   getBillsAllUsers = () => {
     if (this.store) {
       return this.store
